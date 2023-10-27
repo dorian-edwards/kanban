1c6d701c385c5c0986f65ff63994a5196d308079
Dorian Edwards <de.pharmd2018@gmail.com>
2023-10-27 09:34:24 -0400

    customize theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,33 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Plus Jakarta Sans', 'sans-serif'],
+    },
+    colors: {
+      'main-purple': '#635FC7',
+      'main-purple-hover': '#A8A4FF',
+      black: '#000112',
+      'very-dark-gray': '#20212C',
+      'dark-gray': '#2B2C37',
+      'lines-dark': '#3E3F4E',
+      'med-gray': '#828FA3',
+      lavendar: '#E4EBFA',
+      'lgt-gray': '#F4F7FD',
+      whight: '#FFFFFF',
+      red: '#EA5555',
+      'red-hover': '#FF9898',
+    },
+    borderRadius: {
+      sm: '0.4rem',
+      lg: '2.4rem',
+    },
   },
   plugins: [],
 }

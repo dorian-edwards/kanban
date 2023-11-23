1463af9b2a2eb00e8b435beef919529f968ead3d
Dorian Edwards <de.pharmd2018@gmail.com>
2023-11-22 21:13:11 -0500

    fix width of control panel to prevent collapse on close

--- src/components/NavComponents/ControlPanel.tsx
+++ src/components/NavComponents/ControlPanel.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle'
 export default function ControlPanel() {
   return (
     <div className='side-panel-control'>
-      <div className='theme-wrapper w-[90%] max-w-[25.1rem] mx-auto mb-2'>
+      <div className='theme-wrapper w-[90%] max-w-[25.1rem] mx-auto tablet:ml-[1.295rem] desktop:ml-[2.4183rem] mb-2'>
         {' '}
         <ThemeToggle />
       </div>
